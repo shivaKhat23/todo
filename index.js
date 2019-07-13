96a1e4e39e53de1bf1930b225303cd195f145693
Shiva Khatri <shiva.khat238@gmail.com>
2019-07-13 22:20:49 +0200

    add complete,uncomplete and delete actions

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ function createTodo(name) {
 
 let tasks = [
   {
+    id: 0,
     name: "Do homework",
     completed: false
   },
   {
+    id: 1,
     name: "Pay bills",
     completed: false
   },
   {
+    id: 2,
     name: "sleep",
     completed: true
   },
   {
+    id: 3,
     name: "wake up",
     completed: true
   }
 ];
 
-let uncompletedList = document.querySelector(".uncompleted-tasks ul");
-let completedList = document.querySelector(".completed-tasks ul");
-
-function render(element, tasks, completedCheck) {
+function renderItems(element, tasks, completedCheck) {
   element.innerHTML = tasks
     .filter(completedCheck)
     .map(task => {
@@ -44,16 +45,52 @@ function render(element, tasks, completedCheck) {
 function getActions(task) {
   if (!task.completed) {
     return `
-       <i class="fa fa-check-square"></i>
-       <i class="fa fa-trash-alt"></i>
+       <i id="${task.id}" class="fa fa-check-square completeAction"></i>
+       <i id="${task.id}" class="fa fa-trash-alt deleteAction"></i>
     `;
   } else {
     return `
-       <i class="fa fa-undo"></i>
-       <i class="fa fa-trash-alt"></i>
+       <i id="${task.id}" class="fa fa-undo unCompleteAction"></i>
+       <i id="${task.id}" class="fa fa-trash-alt deleteAction"></i>
     `;
   }
 }
 
-render(completedList, tasks, task => task.completed);
-render(uncompletedList, tasks, task => !task.completed);
+function completeItem(event) {
+  let taskToComplete = tasks.find(task => task.id == getId(event));
+  taskToComplete.completed = true;
+  render(tasks);
+}
+
+function deleteItem(event) {
+  tasks = tasks.filter(task => task.id != getId(event));
+  render(tasks);
+}
+
+function unCompleteItem(event) {
+  let taskToUnComplete = tasks.find(task => task.id == getId(event));
+  taskToUnComplete.completed = false;
+  render(tasks);
+}
+
+const getId = (event) => {
+  return Number(event.target.id);
+}
+
+function render(tasks) {
+  let uncompletedList = document.querySelector(".uncompleted-tasks ul");
+  let completedList = document.querySelector(".completed-tasks ul");
+  renderItems(completedList, tasks, task => task.completed);
+  renderItems(uncompletedList, tasks, task => !task.completed);
+  Array.from(document.querySelectorAll("div.actions i.completeAction")).forEach(el =>
+    el.addEventListener("click", completeItem)
+  );
+  Array.from(document.querySelectorAll("div.actions i.deleteAction")).forEach(el =>
+    el.addEventListener("click", deleteItem)
+  );
+  Array.from(document.querySelectorAll("div.actions i.unCompleteAction")).forEach(el =>
+    el.addEventListener("click", unCompleteItem)
+  );
+}
+
+render(tasks);
